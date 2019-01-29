9961cd7ccc3b4f069a4dac1f965a58481ce5625c
owen7788 <150827278@qq.com>
2019-01-29 16:48:35 +0800

    新加了pulls

--- test_web/views.py
+++ test_web/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render
 from django.shortcuts import  HttpResponse
 # 将请求定位到index.html文件中
 def index(request):
+    #业务逻辑对应着不同的业务逻辑，这里转到一个页面。
+    #if request.method == "POST ":
+    #   username = request.POST.get("username",None)
+    #   password = request.POST.get("username", None)
+    #   print(username,password)
     return render(request,'index.html')
 def index2(request):
     return render(request,'index2.html')
